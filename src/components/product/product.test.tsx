147d813449d6c73e6607099d7702f678125124dc
Mohamed Rashid <ge_rashid@hotmail.co.uk>
2021-03-14 01:43:08 +0000

    Implement Product component with title, image and price

--- src/components/product/product.test.tsx
+++ src/components/product/product.test.tsx
@@ -2,9 +2,19 @@ import React from "react";
 import Product from './product';
 import { shallow } from 'enzyme';
 
-test('renders the product if it passed', () => {
-    const productWrapper = shallow(<Product/>);
+test('renders the product if its passed', () => {
+    const props = {
+        productId: '12345',
+        title: "Bosch Serie 4 SMS46MI01G Freestanding Dishwasher, Grey",
+        price: "529.00",
+        image: "//johnlewis.scene7.com/is/image/JohnLewis/238008957?"
+    };
+
+    const productWrapper = shallow(<Product {...props} />);
 
     expect(productWrapper).toHaveLength(1);
     expect(productWrapper.find('.product')).toHaveLength(1);
+    expect(productWrapper.find('.product-title')).toHaveLength(1);
+    expect(productWrapper.find('.product-price')).toHaveLength(1);
+    expect(productWrapper.find('img')).toHaveLength(1);
 });
--- src/components/product/product.tsx
+++ src/components/product/product.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+interface ProductProps {
+    productId: string
+    image: string
+    title: string
+    price: string
+}
 
-const Product = () => (
-    <div className="product">My Product</div>
-);
+const Product = ({ image, title, price}:ProductProps) => {
+    return (
+        <div className="product">
+            <img src={image} />
+            <div className="product-title">{title}</div>
+            <div className="product-price">{price}</div>
+        </div>
+    )
+};
 
 export default Product;
