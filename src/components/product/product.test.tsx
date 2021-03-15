7772b520ef4e9304bba557f140d967ca1621c099
Mohamed Rashid <ge_rashid@hotmail.co.uk>
2021-03-15 17:58:31 +0000

    Re write product tests

--- src/components/product/product.test.tsx
+++ src/components/product/product.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Product from './product';
 import { shallow } from 'enzyme';
 
-test('renders the product if its passed', () => {
+test('renders the product', () => {
     const props = {
         productId: '12345',
         title: "Bosch Serie 4 SMS46MI01G Freestanding Dishwasher, Grey",
@@ -13,12 +13,22 @@ test('renders the product if its passed', () => {
 
     expect(productWrapper).toHaveLength(1);
     expect(productWrapper.find('.product')).toHaveLength(1);
-    expect(productWrapper.find('.product-title')).toHaveLength(1);
-    expect(productWrapper.find('.product-price')).toHaveLength(1);
-    expect(productWrapper.find('img')).toHaveLength(1);
 });
 
-test('renders the price in the correct format', () => {
+test('renders the title of the product', () => {
+    const props = {
+        productId: '12345',
+        title: "Bosch Serie 4 SMS46MI01G Freestanding Dishwasher, Grey",
+        price: "529.00",
+        image: "//johnlewis.scene7.com/is/image/JohnLewis/238008957?"
+    };
+    const productWrapper = shallow(<Product {...props} />);
+
+    expect(productWrapper.find('.product-title').text()).toEqual('Bosch Serie 4 SMS46MI01G Freestanding Dishwasher, Grey');
+});
+
+
+test('renders the price of the product', () => {
     const props = {
         productId: '12345',
         title: "Bosch Serie 4 SMS46MI01G Freestanding Dishwasher, Grey",
@@ -29,3 +39,16 @@ test('renders the price in the correct format', () => {
 
     expect(productWrapper.find('.product-price').text()).toEqual('£529.00');
 });
+
+test('renders the image of the product', () => {
+    const props = {
+        productId: '12345',
+        title: "Bosch Serie 4 SMS46MI01G Freestanding Dishwasher, Grey",
+        price: "529.00",
+        image: "//johnlewis.scene7.com/is/image/JohnLewis/238008957?"
+    };
+    const productWrapper = shallow(<Product {...props} />);
+
+    expect(productWrapper.find('img').prop('src')).toEqual('//johnlewis.scene7.com/is/image/JohnLewis/238008957?');
+});
+
