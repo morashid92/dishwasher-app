21966423e0b39c64977b663ada52e48885d6da1d
Mohamed Rashid <ge_rashid@hotmail.co.uk>
2021-03-18 22:35:52 +0000

    Write more browser tests for Product page

--- client/src/components/product/product.tsx
+++ client/src/components/product/product.tsx
@@ -12,13 +12,15 @@ interface ProductProps {
 
 const Product: React.FC<ProductProps> = ({ image, title, price, productId}) => {
     return (
-        <div className="product">
-            <img className="product-image" src={image} alt="image of dishwasher" />
-            <div className="product-description" >
-                <Link to={`/product/${productId}`}><div tabIndex={1} className="product-title">{title}</div></Link>
-                <Price price={price} floatLeft />
+        <Link to={`/product/${productId}`}>
+            <div className="product">
+                <img className="product-image" src={image} alt="image of dishwasher" />
+                <div className="product-description" >
+                    <div tabIndex={1} className="product-title">{title}</div>
+                    <Price price={price} floatLeft />
+                </div>
             </div>
-        </div>
+        </Link>
     )
 };
 
--- client/src/end-to-end/product-page.spec.js
+++ client/src/end-to-end/product-page.spec.js
@@ -11,6 +11,37 @@ describe(
 
             expect(count).toBe(20);
         });
+
+        it('Opens a product details page when clicking on a product', async () => {
+            const firstProductSelector = '[class="container"] > :first-child';
+            await page.goto('http://localhost:4000');
+            await page.waitForTimeout(4000);
+
+            await page.click(firstProductSelector);
+
+            const productDetailTitle = await page.$eval(".product-details-title", el => el.textContent);
+            expect(productDetailTitle).toEqual('Bosch Serie 4 SMV46JX01G Fully Integrated Dishwasher');
+        });
+
+        it('Contains correct product id in url when clicking on a product', async () => {
+            const firstProductSelector = '[class="container"] > :first-child';
+            await page.goto('http://localhost:4000');
+            await page.waitForTimeout(4000);
+
+            await page.click(firstProductSelector);
+
+            expect(page.url()).toContain('4215447');
+        });
+
+        it('Contains correct title for product page', async () => {
+            await page.goto('http://localhost:4000');
+            await page.waitForTimeout(4000);
+
+            const productDetailTitle = await page.$eval("title", el => el.textContent);
+            expect(productDetailTitle).toEqual('Dishwashers');
+        });
     },
     timeout,
 );
+
+//write test for render of paragraph on product details page
